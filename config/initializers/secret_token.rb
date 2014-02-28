461e5610ea35c7d409e59d1550406f2a4fab1228
pishilong <pishilong@gmail.com>
2014-02-28 21:01:37 +0800

    init RAILS project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DarkTea::Application.config.secret_key_base = 'f1016ddd0b1a01b1eea26f83ac47d2c050c8b30e03d83164797f3385cb37e7d89fa36e1bd97e9ffdb668e59385c4efb79ece785a4fc47c432880abaabed9c46b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DarkTea::Application.config.session_store :cookie_store, key: '_dark-tea_session'
